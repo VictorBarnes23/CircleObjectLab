5d9f074933673eeb33cad03cba4fd6e2b4c57ab3
Victor <vicpbarnes17@gmail.com>
2024-02-05 09:40:19 -0500

    first

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    internal class Circle
+    {
+         //Properties
+        private double Radius {  get; set; }
+        //Constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+        //methods
+        public double CalculateDiameter()
+        {
+            return Radius * 2;
+        }
+        public double CalculateCircumfrence()
+        {
+            return Radius * Math.PI * 2;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public void Grow()
+        {
+            Radius = Radius * 2;
+        }
+        public double GetRadius()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,43 @@
+﻿using CircleObjectsLab;
+
+Console.WriteLine("Please eneter the radius of the circle");
+int radiusValue = -1;
+while(!int.TryParse(Console.ReadLine(), out  radiusValue)|| radiusValue <= 0)
+{
+    Console.WriteLine("Invalid input. Try again");
+}
+
+//Make Circle
+Circle c = new Circle(radiusValue);
+
+bool growCircle = true;
+while (growCircle)
+{
+    //Math
+
+    Console.WriteLine($"Diameter: {c.CalculateDiameter()}");
+    Console.WriteLine($"Circumference: {c.CalculateCircumfrence()}");
+    Console.WriteLine($"Area: {c.CalculateArea()}");
+
+    //continue
+    while (true)
+    {
+        Console.WriteLine("Would you like to grow your circle? yes/no");
+        string answer = Console.ReadLine().ToLower().Trim();
+        if (answer == "yes")
+        {
+            c.Grow();
+            break;
+        }
+        else if (answer == "no")
+        {
+            Console.WriteLine($"Goodbye. Your circles info was {c.GetRadius()}");
+            growCircle = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Not a proper answer try again.");
+        }
+    }
+}
